c0d7d946919ab06c6df967a98ce1b874802f37a0
noahward <noahward@shaw.ca>
2021-03-11 11:04:40 -0800

    Worked on task 4

--- analysis/noah/scripts/.ipynb_checkpoints/functions_libs-checkpoint.py
+++ analysis/noah/scripts/.ipynb_checkpoints/functions_libs-checkpoint.py
@@ -7,7 +7,6 @@ def process_data(path_to_df):
         pd.read_csv(path_to_df, header=None)
         .rename(columns = {0: 'Age', 1: "Workclass", 2: "Final Weight", 3: "Education", 4: "Education Num", 5: "Marital Status",  6: "Occupation", 7: "Relationship", 8: "Race", 9: "Sex", 10: "Capital Gain", 11: "Capital Loss", 12: "Hours per Week", 13: "Native Country", 14: "Salary"})
         .fillna(method="ffill")
-        .drop(['Education Num'])
     )
         
     return df1
